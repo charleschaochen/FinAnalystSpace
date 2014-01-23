c0a1c8177b2c3e5ff060ba3a9c55cb132553d815
charleschaochen <399931011@qq.com>
2014-01-23 14:08:42 +0800

    Initial commit

---
+++ FinAnalyst/src/org/charlestech/fin/test/Test.java
@@ -0,0 +1,13 @@
+package org.charlestech.fin.test;
+
+/*
+ * @Description Just for simple testing
+ * @author Charles Chen
+ * @date 14-1-23
+ * @version 1.0
+ */
+public class Test {
+    public static void main(String[]args){
+
+    }
+}
